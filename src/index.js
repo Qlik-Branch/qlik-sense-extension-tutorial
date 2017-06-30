8d18dee3baff83408a357b0546b274113b586594
John Bellizzi <jabellizzi406@gmail.com>
2017-06-30 13:34:52 -0400

    implementing scrolly images

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import './css/sidebar-style.css';
 import './css/content-style.css';
+import * as d3 from 'd3';
+import {graphScroll} from 'graph-scroll';
+
 
 var hamburgerButton = document.querySelector('.hamburger-open');
 var closeButton = document.querySelector('.sidebar .closebtn');
@@ -11,3 +14,9 @@ hamburgerButton.addEventListener('click', () =>{
 closeButton.addEventListener('click', () =>{
   document.getElementById('sidebar').classList.remove('active');
 })
+graphScroll()
+    .graph(d3.select('#ext-sunburst'))
+    .container(d3.select('#section-0'))
+  .sections(d3.selectAll('#body-left'))
+  // .on('active', function(d){console.log('section ' +d +' active')})
+  // .offset(500);
