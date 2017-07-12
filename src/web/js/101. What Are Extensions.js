b3b0c4d3a4c0d963e9bf9914f52561ae1db5539e
John Bellizzi <jabellizzi406@gmail.com>
2017-07-12 02:19:59 -0400

    section 102 scroll alignment

--- src/web/js/101. What Are Extensions.js
+++ src/web/js/101. What Are Extensions.js
@@ -1,19 +1,20 @@
 import activateSidebar from './sidebar.js';
 import scrollygraph from './scrollygraph.js';
 
+import senseSankeyDesktop from './101/senseSankeyDesktop.js';
+import senseSankeyServer from './101/senseSankeyServer.js';
 import '../sass/101. What Are Extensions.scss';
 
 var sectionList = [
-  '#section-1',
-  '#section-2',
-  '#section-5',
-  '#section-6',
-  '#section-7',
-  '#section-9',
-  '#section-10',
-  '#section-11'
+  '.ext-sunburst',
+  '.ext-navigation',
+  '.ext-resize'
 ];
 
-window.onload = function(){scrollygraph(sectionList)}
 // ============== Sidebar ==============
 activateSidebar(1);
+
+
+scrollygraph(sectionList);
+senseSankeyDesktop('.sense-sankey-desktop');
+senseSankeyServer('.sense-sankey-server');
---
+++ src/web/js/101/senseSankeyDesktop.js
@@ -0,0 +1,100 @@
+import * as d3 from 'd3';
+import {graphScroll} from 'graph-scroll';
+import '../../sass/101/sense-sankey-desktop.scss';
+
+export default function senseSankeyDesktop(section){
+  /* Add an invisible line width 0 width to use as the scrolling section
+      in graph-scroll */
+  var scrollLine = document.createElement('div');
+  scrollLine.classList.add('scroll-line');
+  var scrollLineContent = document.createElement('div');
+  scrollLine.appendChild(scrollLineContent);
+  document.querySelector(section).appendChild(scrollLine);
+
+
+  window.addEventListener('load', function(){
+    graphScroll()
+      .container(d3.select(section))
+      .graph(d3.select(section +' .row'))
+      .sections(d3.selectAll(section +' .scroll-line'));
+  })
+
+
+  // ============== Scroll ==============
+  var img1 = document.querySelector(section + ' .graph > img:first-of-type');
+  var img2 = document.querySelector(section + ' .graph > img:nth-of-type(2)');
+  var img3 = document.querySelector(section + ' .graph > img:nth-of-type(3)');
+  
+  var scrollFunctionArray = [],
+      sectionTop;
+  /* Add scroll event listener to window and pass it array of scroll functions */
+  window.addEventListener('scroll', function(){onscroll(scrollFunctionArray)})
+  /* Function to be called everytime there is scroll action. The function is
+      passed an array of functions, each of which updates the style or attribute
+      using a scale */
+  function onscroll(scrollFunctionArray){
+    // Get top position of section
+    sectionTop = document.querySelector(section).getBoundingClientRect().top;
+
+    // Execute each function in the function array
+    scrollFunctionArray.forEach((fx) =>{
+      fx();
+    });
+    
+    if(sectionTop > -1000){
+      img1.style.display = 'block';
+      img2.style.display = 'none';
+      img3.style.display = 'none';
+    } else if(sectionTop > -2000){
+      img1.style.display = 'none';
+      img2.style.display = 'block';
+      img3.style.display = 'none';
+    } else if(sectionTop > -3000){
+      img1.style.display = 'none';
+      img2.style.display = 'none';
+      img3.style.display = 'block';
+    }
+  }
+
+  /* Function to add a scaling function to function array. This function
+      generates a scale and defines how the element property should be
+      updated in relation to that scale */
+  function addScrollListener(d3Element, type, property, domain, range){
+    // Define the scale
+    var scale = d3.scaleLinear()
+      .domain(domain)
+      .range(range)
+      .clamp(true);
+
+    // Add the scaling effect to the function array
+    scrollFunctionArray.push(function scroll(){
+      d3Element[type](property, scale(sectionTop));
+    });
+  }
+
+  // Imgs
+  addScrollListener(d3.select(img1), 'style', 'opacity', [-100, -400, -700, -1000], [0, 1, 1, 0]);
+  addScrollListener(d3.select(img2), 'style', 'opacity', [-1100, -1400, -1700, -2000], [0, 1, 1, 0]);
+  addScrollListener(d3.select(img3), 'style', 'opacity', [-2100, -2400], [0, 1]);
+
+  // Paragraph
+  addScrollListener(d3.select(section +' .body-left ol > li:first-of-type'),
+    'style', 'opacity', [0, -100], [0, 1]);
+  addScrollListener(d3.select(section +' .body-left ol > li:first-of-type'),
+    'style', 'color', [-1000, -1100], ['#565555', '#ddd']);
+  addScrollListener(d3.select(section +' .body-left ol > li:first-of-type p'),
+    'style', 'color', [-1000, -1100], ['#565555', '#ddd']);
+
+  addScrollListener(d3.select(section +' .body-left ol > li:nth-of-type(2)'),
+    'style', 'opacity', [-1000, -1100], [0, 1]);
+  addScrollListener(d3.select(section +' .body-left ol > li:nth-of-type(2)'),
+    'style', 'color', [-2000, -2100], ['#565555', '#ddd']);
+  addScrollListener(d3.select(section +' .body-left ol > li:nth-of-type(2) p'),
+    'style', 'color', [-2000, -2100], ['#565555', '#ddd']);
+
+  addScrollListener(d3.select(section +' .body-left ol > li:nth-of-type(3)'),
+    'style', 'opacity', [-2000, -2100], [0, 1]);
+
+
+  onscroll(scrollFunctionArray);
+}
