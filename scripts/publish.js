07444beff6dc0282aac8b2bcf00c38a5213e2efd
John Bellizzi <jabellizzi406@gmail.com>
2017-07-13 10:01:55 -0400

    added publish script

---
+++ scripts/publish.js
@@ -0,0 +1,15 @@
+var fs = require('fs-extra');
+var path = require('path');
+
+var folderSource = path.join(__dirname, '../dist'),
+    folderDest = '/Users/johnbellizzi/Dropbox/Small Victories/party hat pants';
+
+fs.emptyDir(folderDest, (err) =>{
+  if(err) return console.log(err);
+
+  console.log('success');
+
+  fs.copy(folderSource, folderDest, function(err){
+    if(err) throw err;
+  })
+})
