f34e0f812b9996555dd2742d908afd73a6d87b97
John Bellizzi <jabellizzi406@gmail.com>
2017-07-13 13:00:14 -0400

    style updates to section 103

--- src/web/js/103. Make it Data Driven.js
+++ src/web/js/103. Make it Data Driven.js
@@ -3,6 +3,7 @@ import {scrollPosition, scrollOpacity} from './scrollygraph.js';
 import aceEditor from './ace-editor.js';
 
 import testingOurExtension from './103/lifecycle-of-extension.js';
+import dataPages from './103/data-pages.js';
 import '../sass/103. Make it Data Driven.scss';
 
 var sectionList = [
@@ -26,10 +27,18 @@ var sectionList = [
   '.ext-table'
 ];
 
+var scrollPositionList = [
+	'.initial-properties-editor',
+	'.definition-editor'
+]
+
 // ============== Sidebar ==============
 activateSidebar(3);
 
+scrollPosition(scrollPositionList);
+
 testingOurExtension('.lifecycle-of-extension');
+dataPages('.data-pages');
 
 
 // document.querySelector('body').onload = function(){
@@ -37,171 +46,171 @@ testingOurExtension('.lifecycle-of-extension');
 // }
 
 
-// // Initial Properties
-// var initialProperties = 
-// `var myProps = {
-// 	textColor: "black",
-// 	qHyperCubeDef: {
-// 		qDimensions: [],
-// 		qMeasures: [],
-// 		qInitialDataFetch: [
-// 			{
-// 				qTop: 0,
-// 				qLeft: 0,
-// 				qWidth: 10,
-// 				qHeight: 1000
-// 			}
-// 		]
-// 	}
-// };`
+// Initial Properties
+var initialProperties = 
+`var myProps = {
+	textColor: "black",
+	qHyperCubeDef: {
+		qDimensions: [],
+		qMeasures: [],
+		qInitialDataFetch: [
+			{
+				qTop: 0,
+				qLeft: 0,
+				qWidth: 10,
+				qHeight: 1000
+			}
+		]
+	}
+};`
 
-// aceEditor('initial-properties-editor', 'javascript', initialProperties);
+aceEditor('initial-properties-editor', 'javascript', initialProperties, 'my-table.js');
 
-// // Definition
-// var definition =
-// `var myDefinition = {
-//     type: "items",
-//     component: "accordion",
-//     items: {
-//         dimensions: {
-//             uses: "dimensions",
-//             min: 1,
-//             max: 5
-//         },
-//         settings: {
-//             uses: "settings"
-//         }
-//     }
-// };`
+// Definition
+var definition =
+`var myDefinition = {
+    type: "items",
+    component: "accordion",
+    items: {
+        dimensions: {
+            uses: "dimensions",
+            min: 1,
+            max: 5
+        },
+        settings: {
+            uses: "settings"
+        }
+    }
+};`
 
-// aceEditor('definition-editor', 'javascript', definition);
+aceEditor('definition-editor', 'javascript', definition, 'my-table.js');
 
-// // Definition 2
-// var definition2 =
-// `var myDefinition = {
-//     type: "items",
-//     component: "accordion",
-//     items: {
-//         dimensions: {
-//             uses: "dimensions",
-//             min: 1,
-//             max: 5
-//         },
-//         measures: {
-//             uses: "measures",
-//             min: 0,
-//             max: 5
-//         },
-//         sorting : {
-//             uses: "sorting"
-//         },
-//         settings: {
-//             uses: "settings"
-//         }
-//     }
-// };`
+// Definition 2
+var definition2 =
+`var myDefinition = {
+    type: "items",
+    component: "accordion",
+    items: {
+        dimensions: {
+            uses: "dimensions",
+            min: 1,
+            max: 5
+        },
+        measures: {
+            uses: "measures",
+            min: 0,
+            max: 5
+        },
+        sorting : {
+            uses: "sorting"
+        },
+        settings: {
+            uses: "settings"
+        }
+    }
+};`
 
-// aceEditor('definition-editor-2', 'javascript', definition2);
+aceEditor('definition-editor-2', 'javascript', definition2, 'my-table.js');
 
-// // Paint
-// var paint =
-// `var myPaint = function($element, layout) {
+// Paint
+var paint =
+`var myPaint = function($element, layout) {
 	
-// // Clear the previous contents of the container so we start from scratch each time
-// 	$element.html("");
-// }`
+// Clear the previous contents of the container so we start from scratch each time
+	$element.html("");
+}`
 
-// aceEditor('paint-editor', 'javascript', paint);
+aceEditor('paint-editor', 'javascript', paint, 'my-table.js');
 
-// // Paint 2
-// var paint2 =
-// `var myPaint = function($element, layout) {
+// Paint 2
+var paint2 =
+`var myPaint = function($element, layout) {
 	
-// // Clear the previous contents of the container so we start from scratch each time
-// 	$element.html("");
+// Clear the previous contents of the container so we start from scratch each time
+	$element.html("");
 
-// 	// Create a table
-// 	var table = document.createElement("table");
+	// Create a table
+	var table = document.createElement("table");
 	
-// 	// Create a header row
-// 	var hRow = document.createElement("tr");
-// }`
+	// Create a header row
+	var hRow = document.createElement("tr");
+}`
 
-// aceEditor('paint-editor-2', 'javascript', paint2);
+aceEditor('paint-editor-2', 'javascript', paint2, 'my-table.js');
 
-// // Paint 3
-// var paint3 =
-// `var myPaint = function($element, layout) {
-// // Clear the previous contents of the container so we start from scratch each time
-// 	$element.html("");
+// Paint 3
+var paint3 =
+`var myPaint = function($element, layout) {
+// Clear the previous contents of the container so we start from scratch each time
+	$element.html("");
 
-// 	// Create a table
-// 	var table = document.createElement("table");
+	// Create a table
+	var table = document.createElement("table");
 	
-// 	// Create a header row
-// 	var hRow = document.createElement("tr");
+	// Create a header row
+	var hRow = document.createElement("tr");
 
-// 	// Add dimension labels
-// 	var dimensionInfo = layout.qHyperCube.qDimensionInfo;
-// 	for(var i = 0; i < dimensionInfo.length; i++) {
-// 		// Create a header cell
-// 		var hCell = document.createElement("th");
-// 		// Set the cell contents to the dimension label
-// 		hCell.innerHTML = dimensionInfo[i].qFallbackTitle;
-// 		// Add the cell to the header row
-// 		hRow.appendChild(hCell);
-// 	}
+	// Add dimension labels
+	var dimensionInfo = layout.qHyperCube.qDimensionInfo;
+	for(var i = 0; i < dimensionInfo.length; i++) {
+		// Create a header cell
+		var hCell = document.createElement("th");
+		// Set the cell contents to the dimension label
+		hCell.innerHTML = dimensionInfo[i].qFallbackTitle;
+		// Add the cell to the header row
+		hRow.appendChild(hCell);
+	}
 
-// 	// Add measure labels
-// 	var measureInfo = layout.qHyperCube.qMeasureInfo;
-// 	for(var i = 0; i < measureInfo.length; i++) {
-// 		// Create a header cell
-// 		var hCell = document.createElement("th");
-// 		// Set the cell contents to the measure label
-// 		hCell.innerHTML = measureInfo[i].qFallbackTitle;
-// 		// Add the cell to the header row
-// 		hRow.appendChild(hCell);
-// 	}
+	// Add measure labels
+	var measureInfo = layout.qHyperCube.qMeasureInfo;
+	for(var i = 0; i < measureInfo.length; i++) {
+		// Create a header cell
+		var hCell = document.createElement("th");
+		// Set the cell contents to the measure label
+		hCell.innerHTML = measureInfo[i].qFallbackTitle;
+		// Add the cell to the header row
+		hRow.appendChild(hCell);
+	}
 
-// 	// Add the header row to the table
-// 	table.appendChild(hRow);
+	// Add the header row to the table
+	table.appendChild(hRow);
 
-// 	// Append the table to the $element
-// 	$element.append(table);
-// }`
+	// Append the table to the $element
+	$element.append(table);
+}`
 
-// aceEditor('paint-editor-3', 'javascript', paint3);
+aceEditor('paint-editor-3', 'javascript', paint3, 'my-table.js');
 
-// // Paint 4
-// var paint4 =
-// `var myPaint = function($element, layout) {
-// 	// ...
-// 	var qMatrix = layout.qHyperCube.qDataPages[0].qMatrix;
+// Paint 4
+var paint4 =
+`var myPaint = function($element, layout) {
+	// ...
+	var qMatrix = layout.qHyperCube.qDataPages[0].qMatrix;
 
-// 	// Iterate through each row of the qMatrix
-// 	for(var row = 0; row < qMatrix.length; row++) {
-// 		// Get current row data
-// 		var currentRow = qMatrix[row];
-// 		// Create a row
-// 		var tr = document.createElement("tr");
-// 		// Iterate through each column of the row
-// 		for(var col = 0; col < currentRow.length; col++) {
-// 			// Get current cell data
-// 			var currentCell = currentRow[col];
-// 			// Create a cell
-// 			var td = document.createElement("td");
-// 			// Add text value to the cell
-// 			td.innerHTML = currentCell.qText;
-// 			// Append the cell to the row
-// 			tr.appendChild(td);
-// 		}
-// 		// append the row to the table
-// 		table.appendChild(tr);
-// 	}
+	// Iterate through each row of the qMatrix
+	for(var row = 0; row < qMatrix.length; row++) {
+		// Get current row data
+		var currentRow = qMatrix[row];
+		// Create a row
+		var tr = document.createElement("tr");
+		// Iterate through each column of the row
+		for(var col = 0; col < currentRow.length; col++) {
+			// Get current cell data
+			var currentCell = currentRow[col];
+			// Create a cell
+			var td = document.createElement("td");
+			// Add text value to the cell
+			td.innerHTML = currentCell.qText;
+			// Append the cell to the row
+			tr.appendChild(td);
+		}
+		// append the row to the table
+		table.appendChild(tr);
+	}
 
-// 	// Append the table to the $element
-// 	$element.appendChild(table);
+	// Append the table to the $element
+	$element.appendChild(table);
 
-// }`;
+}`;
 
-// aceEditor('paint-editor-4', 'javascript', paint4);
+aceEditor('paint-editor-4', 'javascript', paint4, 'my-table.js');
---
+++ src/web/js/103/data-pages.js
@@ -0,0 +1,118 @@
+import * as d3 from 'd3';
+import {graphScroll} from 'graph-scroll';
+import '../../sass/103/data-pages.scss';
+
+export default function dataPages(section){
+  /* Add an invisible line width 0 width to use as the scrolling section
+      in graph-scroll */
+  var scrollLine = document.createElement('div');
+  scrollLine.classList.add('scroll-line');
+  var scrollLineContent = document.createElement('div');
+  scrollLine.appendChild(scrollLineContent);
+  document.querySelector(section).appendChild(scrollLine);
+
+  window.addEventListener('load', function(){
+    graphScroll()
+      .container(d3.select(section))
+      .graph(d3.select(section +' .row'))
+      .sections(d3.selectAll(section +' .scroll-line'));
+  })
+
+
+  // ============== Scroll ==============
+  var scrollFunctionArray = [],
+      sectionTop = document.querySelector(section).getBoundingClientRect().top;
+
+  var imgs = d3.selectAll(section +' .graph > img');
+  var paragraphs = d3.selectAll(section +' .body-left p');
+  var imgData = [], 
+      paragraphData = [];
+
+  function setImgData(imgArray){
+    imgArray.forEach((img, i) =>{
+      if(i === 0){ // if first img..
+        // Set display
+        if(sectionTop > -1000){
+          imgData[i] = 'block';
+        } else imgData[i] = 'none';
+      } else if((i + 1) === imgArray.length){ // if last img..
+        if(sectionTop <= (-1000*(i + 1) + 1000)){
+          imgData[i] = 'block';
+        } else imgData[i] = 'none';
+      } else {
+        if(sectionTop > -1000*(i + 1) && sectionTop <= (-1000*(i + 1) + 1000)){
+          imgData[i] = 'block';
+        } else imgData[i] = 'none';
+      }
+    })
+  };
+
+  function setImgOpacity(imgArray){
+    imgArray.forEach((img, i) =>{
+      if((i + 1) === imgArray.length){ // if last img..
+        // Set opacity
+        addScrollListener(d3.select(img), 'style', 'opacity', 
+          [(-i*1000 - 100), (-i*1000 - 400)], [0, 1]);
+      } else {
+        // Set opacity
+        addScrollListener(d3.select(img), 'style', 'opacity', 
+          [(-i*1000 - 100), (-i*1000 - 400), (-i*1000 - 700), (-i*1000 - 1000)], [0, 1, 1, 0]);
+      }
+    })
+  } setImgOpacity(imgs._groups[0]);
+
+  function setParagraphColor(pArray){
+    pArray.forEach((p, i) =>{
+      if((i + 1) === pArray.length){ // if last p..
+        // Set Color
+        addScrollListener(d3.select(p), 'style', 'color',
+          [(-i*1000 - 100), (-i*1000 - 400)], ['#ddd', '#565555']);
+      } else{
+        // Set color
+        addScrollListener(d3.select(p), 'style', 'color',
+          [(-i*1000 - 100), (-i*1000 - 400), (-i*1000 - 700), (-i*1000 - 1000)], 
+          ['#ddd', '#565555', '#565555', '#ddd']);
+      }
+    })
+  } setParagraphColor(paragraphs._groups[0]);
+
+  
+  /* Add scroll event listener to window and pass it array of scroll functions */
+  window.addEventListener('scroll', function(){onscroll(scrollFunctionArray)})
+  /* Function to be called everytime there is scroll action. The function is
+      passed an array of functions, each of which updates the style or attribute
+      using a scale */
+  function onscroll(scrollFunctionArray){
+    // Get top position of section
+    sectionTop = document.querySelector(section).getBoundingClientRect().top;
+    setImgData(imgs._groups[0]);
+    imgs.data(imgData);
+
+    // Execute each function in the function array
+    scrollFunctionArray.forEach((fx) =>{
+      fx();
+    });
+    
+    imgs.style('display', (d, i) =>{
+      return d;
+    })
+  } onscroll(scrollFunctionArray);
+
+
+  /* Function to add a scaling function to function array. This function
+      generates a scale and defines how the element property should be
+      updated in relation to that scale */
+  function addScrollListener(d3Element, type, property, domain, range){
+    // Define the scale
+    var scale = d3.scaleLinear()
+      .domain(domain)
+      .range(range)
+      .clamp(true);
+
+    // Add the scaling effect to the function array
+    scrollFunctionArray.push(function scroll(){
+      d3Element[type](property, scale(sectionTop));
+    });
+  }
+  
+}
